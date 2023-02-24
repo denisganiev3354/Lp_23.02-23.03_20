6cae22abf2aa0a85ffc611006877852a3cda92f0
denisganiev3354 <denisganiev3354@gmail.com>
2023-02-24 23:13:28 +0400

    Task 03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -3,13 +3,42 @@
 // 3 -> Среда
 // 5 -> Пятница
 
+
 Console.WriteLine("Введите номер: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 3) ;
+if(number == 1)
 {
-    Console.WriteLine("Среда ");
+    Console.WriteLine("Понедельник.");
 }
-    else if (number == 5) ;
+else if(number == 2)
 {
-    Console.WriteLine("Пятница ");
+    Console.WriteLine("Вторник.");
+}
+else if(number == 3)
+{
+    Console.WriteLine("Среда.");
+}
+else if(number == 4)
+{
+    Console.WriteLine("Четверг.");
+}
+else if(number == 5)
+{
+    Console.WriteLine("Пятница.");
+}
+else if(number == 6)
+{
+    Console.WriteLine("Суббота.");
+}
+else if(number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else if(number > 7)
+{
+    Console.WriteLine("Недопустимое значение");
+}
+else if(number < 1)
+{
+    Console.WriteLine("Недопустимое значение");
 }
