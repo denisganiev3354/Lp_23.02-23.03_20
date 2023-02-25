d9ed58732f0585023f48f768cdc89853a90a1211
denisganiev3354 <denisganiev3354@gmail.com>
2023-02-25 17:04:09 +0400

    Task 07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -4,3 +4,17 @@
 // 456 -> 6
 // 782 -> 2
 // 918 -> 8 
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)  number *= -1;
+
+if (number > 99 && number < 1000)
+{
+    int result = number % 10;
+    Console.Write(result);
+}
+else
+{
+    Console.Write("Нужно ввести трехзначное число");
+}
