7235528c07f7fc41b28ecbb3e381ff10e52d3e16
denisganiev3354 <denisganiev3354@gmail.com>
2023-03-06 18:20:05 +0400

    дз №3 Работа над ошибками

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,25 +6,25 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число: ");
+Console.WriteLine("Enter a five-digit number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 bool polyndrom = Polyndrom(number);
 
 if (number > 10000 && number < 99999)
 {
-    Console.WriteLine(polyndrom ? "Да" : "Нет");
+    Console.WriteLine(polyndrom ? "YES" : "NO");
 }
 else
 {
-    Console.WriteLine("Введено недопустимое значание.");
+    Console.WriteLine("ERROR!");
 }
 
 bool Polyndrom(int num)
 {
     int num1 = num / 10000;
-    int num5 = number % 10;
-    int num2 = num / 1000;
+    int num5 = num % 10;
+    int num2 = num / 1000 % 10;
     int num4 = num / 10 % 10;
     if (num1 == num5 && num2 == num4)
     {
