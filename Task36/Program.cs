c67684d3b4e0db1f8aec63f0da1ae1e2a72a648f
denisganiev3354 <denisganiev3354@gmail.com>
2023-03-15 17:46:45 +0400

    new commit

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -5,4 +5,39 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arraySize];
 
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arraySize)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arraySize.Length; i++)
+    {
+        arraySize[i] = rnd.Next(-100, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] SumElemOdd(int[] arr);
+{
+    int sum = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+       
+    }
+    return sum;
+}
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,5 +1,64 @@
-﻿// Задача 38: 
-// 1. Задайте массив вещественных чисел. 
-// 2. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// // Задача 38: 
+// // 1. Задайте массив вещественных чисел. 
+// // 2. Найдите разницу между максимальным и минимальным элементов массива.
 
-// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+// // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+FillArrayRandomNumbers(array);
+Console.Write("Mассив: ");
+PrintArrayDouble(array);
+double searchMax = SearchMax(array);
+double searchMin = SearchMin(array);
+double result = Math.Round((searchMax - searchMin), 1);
+
+Console.WriteLine($"состоит из {array.Length} чисел. Максимальный элемент = {searchMax}, минимальный элемент = {searchMin}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом = {result}");
+
+void FillArrayRandomNumbers(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+    }
+}
+
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double SearchMax(double[] arr)
+{
+    int i = 1;
+    double max = arr[0];
+    while (i < arr.Length)
+    {
+        if (arr[i] > max) max = arr[i];
+        i++;
+    }
+    return max;
+}
+
+double SearchMin(double[] arr)
+{
+    int i = 1;
+    double min = arr[0];
+    while (i < arr.Length)
+    {
+        if (arr[i] < min) min = arr[i];
+        i++;
+    }
+    return min;
+}
+
+
+
