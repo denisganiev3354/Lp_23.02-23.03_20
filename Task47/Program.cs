80cb3cd0066b5ce33052d5ddffbc65b4bd07eb3f
denisganiev3354 <denisganiev3354@gmail.com>
2023-03-20 21:00:02 +0400

    Дз № 7 - Урок7. (Двумерные массивы)

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -7,3 +7,37 @@
 // 1 -3,3 8 -9,9
 
 // 8 7,8 -7,1 9
+
+double[,] CreatMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new.Random.Next(1,100))                   rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(double[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 3}");
+        } 
+        Console.WriteLine("|");
+    }
+   
+}
+ 
