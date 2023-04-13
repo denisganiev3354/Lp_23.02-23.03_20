8fd17c674962ef5f9c0310e4a4aeb52b81d8c829
denisganiev3354 <denisganiev3354@gmail.com>
2023-04-13 23:02:54 +0400

    Дз №9( Урок 9. Рекурсия)

---
+++ Task66/Program.cs
@@ -0,0 +1,23 @@
+﻿// // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($" Cуммa натуральных элементов в промежутке от M до N = {SumNumbers(m, n)}");
+
+int SumNumbers(int a, int b)
+{
+    if (a == 0) return (b * (b + 1)) / 2;            
+    else if (b == 0) return (a * (a + 1)) / 2;       
+    else if (a == b) return a;                       
+    else if (a < b) return b + SumNumbers(a, b - 1); 
+    else return b + SumNumbers(a, b + 1);  
+}
+
+
+  
